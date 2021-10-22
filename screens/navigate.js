9a638d8aeb4ba1e689e65ad3500b5b0191dc7a67
Jason <joshuajason403@gmail.com>
2021-10-22 13:06:06 +0100

    Add updated navigations

--- screens/navigate.js
+++ screens/navigate.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import homePage from "../screens/homePage";
+import login from "../screens/login";
+
+// import Login from "../screens/login";
+// import About from "../screens/about";
+// import Signup from "../screens/signup";
+// import SignInfo from "../screens/signInfo";
+
+const Stack = createNativeStackNavigator();
 
 export default function navigate() {
-    return (
-      <View>
-        <Text>Navigation</Text>
-      </View>
-    );
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+    //   screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="homePage" component={homePage} />
+      <Stack.Screen name="login" component={login} />
+       {/* <Stack.Screen name="Signup" component={Signup} />
+      // <Stack.Screen name="About" component={About} />
+      // <Stack.Screen name="SignInfo" component={SignInfo} /> */}
+    </Stack.Navigator>
+  );
 }
