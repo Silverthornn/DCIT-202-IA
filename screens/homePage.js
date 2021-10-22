ecae3407e5cb4569c9762fc2f8568bb657f28ae6
Jason <joshuajason403@gmail.com>
2021-10-22 10:13:42 +0100

    Add screens folder

---
+++ screens/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function homePage() {
+    return (
+      <View>
+        <Text>This is the home page of the application</Text>
+      </View>
+    );
+}
