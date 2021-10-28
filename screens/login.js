e3c506fdb623a8cc66414424eef6ab9b5af08fe6
Jason <joshuajason403@gmail.com>
2021-10-28 23:15:42 +0100

    login changed

--- screens/login.js
+++ screens/login.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import homePage from './homePage';
+import navigate from './navigate';
 
-export default function login() {
+export default function login({navigation}) {
     return (
       <View>
         <Text>Login or Sign In</Text>
         <TextInput placeholder = "username/email"/>
         <TextInput placeholder = "password" secureTextEntry = "true" autoCapitalize = "false" />
-        <Button title = "login" onPress = {<homePage/>} /> 
+        <Button title = "login" onPress = {()=>navigation.navigate('homePage')} /> 
       </View>
     );
 }
