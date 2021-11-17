796feefe17845574c6562700b11cb12532a24019
Jason <joshuajason403@gmail.com>
2021-11-17 23:58:59 +0000

    Still working on cart and products

--- screens/cart.js
+++ screens/cart.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View, TouchableOpacity} from 'react-native';
-import {ShoppingCartOutlined} from 'antd';
+import { Icon } from 'react-native-elements';
 
 export default function cart(){
     return(
         <View>
-            
+            <Icon name="home" 
+                onPress ={()=>{navigation.navigate("Home")}}
+            />
         </View>
     )
 }
